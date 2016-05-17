8885d3057ef41a762218b984b264270c396efbd6
Brains <n-orr@onu.edu>
2016-05-17 09:05:40 -0400

    New Stuff

---
+++ src/Wedding/Controllers/Web/AppController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wedding.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Directions()
+        {
+            return View();
+        }
+
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
